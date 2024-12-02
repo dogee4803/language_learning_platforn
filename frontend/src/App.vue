03215f2f35b9efdc79ec499e3c987ba0d50220d4
dmitrii.kalenitskii <dogee4803@gmail.com>
2024-12-03 05:13:36 +0800

    Added header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
-import MenuBar from './components/MenuBar.vue';
+import MenuBar from '@/components/MenuBar.vue';
+import AppHeader from '@/components/AppHeader.vue';
 </script>
 
 <template>
-  <div id="app">
+  <div class="min-h-screen bg-gray-50">
+    <AppHeader />
     <MenuBar />
-    <router-view></router-view> <!-- Здесь отображаются страницы -->
+    <main class="py-6">
+      <router-view></router-view> <!-- Здесь отображаются страницы -->
+    </main>
   </div>
 </template>
 
---
+++ frontend/src/components/AppHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <header class="bg-purple-600 text-white py-4 shadow-md">
+    <div class="max-w-[85%] mx-auto">
+      <h1 class="text-2xl font-semibold">ИС для курсов иностранных языков</h1>
+    </div>
+  </header>
+</template>
+
+<script>
+</script>
