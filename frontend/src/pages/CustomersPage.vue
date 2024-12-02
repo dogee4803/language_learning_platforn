fa76e7986fdd53cde602a9d89bc63051a12a681f
dmitrii.kalenitskii <dogee4803@gmail.com>
2024-12-03 02:26:07 +0800

    Added FIO to customers

--- frontend/src/pages/CustomersPage.vue
+++ frontend/src/pages/CustomersPage.vue
@@ -5,9 +5,11 @@
         <Prime-DataTable :value="customers" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]" tableStyle="max-width: 75rem">
             <Prime-Column field="id" header="ID" style="width: 25%"></Prime-Column>
             <Prime-Column field="last_name" header="Фамилия" style="width: 25%"></Prime-Column>
+            <Prime-Column field="first_name" header="Имя" style="width: 25%"></Prime-Column>
+            <Prime-Column field="middle_name" header="Отчество" style="width: 25%"></Prime-Column>
             <Prime-Column field="gender" header="Пол" style="width: 25%"></Prime-Column>
             <Prime-Column field="phone_number" header="Телефон" style="width: 25%"></Prime-Column>
-            <Prime-Column field="birth_date" header="ДР" style="width: 25%"></Prime-Column>
+            <Prime-Column field="birth_date" header="Дата рождения" style="width: 25%"></Prime-Column>
         </Prime-DataTable>
     </div>
   </template>
